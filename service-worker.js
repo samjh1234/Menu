5ccaff11697fa9e9988bd53361447deaddd354b0
samjh1234 <wisamjh@hotmail.com>
2024-11-29 11:35:16 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "menu-cache-v1";
+const urlsToCache = [
+    "./", // Cache the HTML file
+    "./manifest.json",
+    "./icon-192x192.png",
+    "./icon-512x512.png"
+];
+
+// Install Service Worker and Cache Files
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Activate Service Worker and Remove Old Caches
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Cached Files or Make Network Request
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
